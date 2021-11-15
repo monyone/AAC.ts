50337e29c81a878e2953f7b632ba9ad5083b79f6
monyone <monyone.teihen@gmail.com>
2021-11-15 21:50:39 +0900

    fix scale factor using

--- src/spectral_data.ts
+++ src/spectral_data.ts
@@ -27,94 +27,96 @@ export default class SpectralData {
   public constructor(ics_info: ICSInfo, section_data: SectionData, scale_factor_data: ScalefactorData, stream: BitStream) {    
     for (let g = 0; g < ics_info.num_window_groups; g++) {
       for (let i = 0; i < section_data.num_sect[g]; i++) {
-        for (let k = ics_info.sect_sfb_offset[g][section_data.sect_start[g][i]]; k < ics_info.sect_sfb_offset[g][section_data.sect_end[g][i]]; ) {
-          if (section_data.sect_cb[g][i] === HCB.ZERO_HCB || section_data.sect_cb[g][i] === HCB.INTENSITY_HCB || section_data.sect_cb[g][i] === HCB.INTENSITY_HCB2) {
-            this.x_quant.push(0);
-            k++;
-          } else if (section_data.sect_cb[g][i] === HCB.NOISE_HCB) {
-            throw new Error('Not implemented yet.')
-          } else if (section_data.sect_cb[g][i] < HCB.FIRST_PAIR_HCB) {
-            const index = section_data.sect_cb[g][i] - 1;
+        for (let sfb = section_data.sect_start[g][i]; sfb < section_data.sect_end[g][i]; sfb++) {
+          for (let k = ics_info.sect_sfb_offset[g][sfb]; k < ics_info.sect_sfb_offset[g][sfb + 1]; ) {
+            if (section_data.sect_cb[g][i] === HCB.ZERO_HCB || section_data.sect_cb[g][i] === HCB.INTENSITY_HCB || section_data.sect_cb[g][i] === HCB.INTENSITY_HCB2) {
+              this.x_quant.push(0);
+              k++;
+            } else if (section_data.sect_cb[g][i] === HCB.NOISE_HCB) {
+              throw new Error('Not implemented yet.')
+            } else if (section_data.sect_cb[g][i] < HCB.FIRST_PAIR_HCB) {
+              const index = section_data.sect_cb[g][i] - 1;
   
-            let tree: BinarySearchTree | null = HUFFMAN_QUADS[index];
-            while (tree && !tree.isLeaf) {
-              tree = tree.select(stream.readBits(1));
-            }
-            if (!tree) { throw new Error('No Huffman Code Available!'); }
-  
-            let { signed, w, x, y, z } = HUFFMAN_QUADS_FUNC[index](tree.index!);
-            if (!signed) {
-              if (w !== 0) {
-                if (stream.readBool()) { w = -w; }
+              let tree: BinarySearchTree | null = HUFFMAN_QUADS[index];
+              while (tree && !tree.isLeaf) {
+                tree = tree.select(stream.readBits(1));
               }
-              if (x !== 0) {
-                if (stream.readBool()) { x = -x; }
-              }
-              if (y !== 0) {
-                if (stream.readBool()) { y = -y; }
-              }
-              if (z !== 0) {
-                if (stream.readBool()) { z = -z; }
-              }
-            }
+              if (!tree) { throw new Error('No Huffman Code Available!'); }
 
-            this.x_quant.push(IQ(w) * scale_factor_data.scalefactor[section_data.sect_start[g][i]]);
-            this.x_quant.push(IQ(x) * scale_factor_data.scalefactor[section_data.sect_start[g][i]]);
-            this.x_quant.push(IQ(y) * scale_factor_data.scalefactor[section_data.sect_start[g][i]]);
-            this.x_quant.push(IQ(z) * scale_factor_data.scalefactor[section_data.sect_start[g][i]]);
-  
-            k += 4;
-          } else {
-            const index = section_data.sect_cb[g][i] - HCB.FIRST_PAIR_HCB;
-  
-            let tree: BinarySearchTree | null = HUFFMAN_PAIRS[index];
-            while (tree && !tree.isLeaf) {
-              tree = tree.select(stream.readBits(1));
-            }
-            if (!tree) { throw new Error('No Huffman Code Available!'); }
-  
-            let { signed, y, z } = HUFFMAN_PAIRS_FUNC[index](tree.index!);
-  
-            if (!signed) {
-              if (y !== 0) {
-                if (stream.readBool()) { y = -y; }
+              let { signed, w, x, y, z } = HUFFMAN_QUADS_FUNC[index](tree.index!);
+              if (!signed) {
+                if (w !== 0) {
+                  if (stream.readBool()) { w = -w; }
+                }
+                if (x !== 0) {
+                  if (stream.readBool()) { x = -x; }
+                }
+                if (y !== 0) {
+                  if (stream.readBool()) { y = -y; }
+                }
+                if (z !== 0) {
+                  if (stream.readBool()) { z = -z; }
+                }
               }
-              if (z !== 0) {
-                if (stream.readBool()) { z = -z; }
-              }
-            }
+
+              this.x_quant.push(IQ(w) * scale_factor_data.scalefactor[sfb]);
+              this.x_quant.push(IQ(x) * scale_factor_data.scalefactor[sfb]);
+              this.x_quant.push(IQ(y) * scale_factor_data.scalefactor[sfb]);
+              this.x_quant.push(IQ(z) * scale_factor_data.scalefactor[sfb]);
   
-            if (section_data.sect_cb[g][i] === HCB.ESC_HCB) {
-              if (Math.abs(y) === 16) {
-                  let count = 0;
-                  while (true) {
-                    if(!stream.readBool()) { break; }
-                    count++;
-                  }
-                  const escape_word = stream.readBits(count + 4);
-                  const escape_value = sign(y) * (2 ** (count + 4) + escape_word);
-                  this.x_quant.push(IQ(escape_value) * scale_factor_data.scalefactor[section_data.sect_start[g][i]]);
-              } else {
-                this.x_quant.push(IQ(y) * scale_factor_data.scalefactor[section_data.sect_start[g][i]]);
-              }
-              if (Math.abs(z) === 16) {
-                  let count = 0;
-                  while (true) {
-                    if(!stream.readBool()) { break; }
-                    count++;
-                  }
-                  const escape_word = stream.readBits(count + 4);
-                  const escape_value = sign(z) * (2 ** (count + 4) + escape_word);
-                  this.x_quant.push(IQ(escape_value) * scale_factor_data.scalefactor[section_data.sect_start[g][i]]);
-              } else {
-                this.x_quant.push(IQ(z) * scale_factor_data.scalefactor[section_data.sect_start[g][i]]);
-              }
+              k += 4;
             } else {
-              this.x_quant.push(IQ(y) * scale_factor_data.scalefactor[section_data.sect_start[g][i]]);
-              this.x_quant.push(IQ(z) * scale_factor_data.scalefactor[section_data.sect_start[g][i]]);
-            }
+              const index = section_data.sect_cb[g][i] - HCB.FIRST_PAIR_HCB;
   
-            k += 2;
+              let tree: BinarySearchTree | null = HUFFMAN_PAIRS[index];
+              while (tree && !tree.isLeaf) {
+                tree = tree.select(stream.readBits(1));
+              }
+              if (!tree) { throw new Error('No Huffman Code Available!'); }
+  
+              let { signed, y, z } = HUFFMAN_PAIRS_FUNC[index](tree.index!);
+  
+              if (!signed) {
+                if (y !== 0) {
+                  if (stream.readBool()) { y = -y; }
+                }
+                if (z !== 0) {
+                  if (stream.readBool()) { z = -z; }
+                }
+              }
+  
+              if (section_data.sect_cb[g][i] === HCB.ESC_HCB) {
+                if (Math.abs(y) === 16) {
+                    let count = 0;
+                    while (true) {
+                      if(!stream.readBool()) { break; }
+                      count++;
+                    }
+                    const escape_word = stream.readBits(count + 4);
+                    const escape_value = sign(y) * (2 ** (count + 4) + escape_word);
+                    this.x_quant.push(IQ(escape_value) * scale_factor_data.scalefactor[sfb]);
+                } else {
+                  this.x_quant.push(IQ(y) * scale_factor_data.scalefactor[sfb]);
+                }
+                if (Math.abs(z) === 16) {
+                    let count = 0;
+                    while (true) {
+                      if(!stream.readBool()) { break; }
+                      count++;
+                    }
+                    const escape_word = stream.readBits(count + 4);
+                    const escape_value = sign(z) * (2 ** (count + 4) + escape_word);
+                    this.x_quant.push(IQ(escape_value) * scale_factor_data.scalefactor[sfb]);
+                } else {
+                  this.x_quant.push(IQ(z) * scale_factor_data.scalefactor[sfb]);
+                }
+              } else {
+                this.x_quant.push(IQ(y) * scale_factor_data.scalefactor[sfb]);
+                this.x_quant.push(IQ(z) * scale_factor_data.scalefactor[sfb]);
+              }
+  
+              k += 2;
+            }
           }
         }
       }
