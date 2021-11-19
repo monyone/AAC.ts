8974ef4f9e44eae2521040b9a51b6492a63b7b59
monyone <monyone.teihen@gmail.com>
2021-11-20 02:43:45 +0900

    remove naive mdct methods

--- src/mdct.ts
+++ src/mdct.ts
@@ -105,3 +105,21 @@ export const imdct = (frequencies: number[]) => {
 
   return samples;
 }
+
+/* naive solution 
+export const imdct = (frequencies: number[]) => {
+  const n = frequencies.length;
+  const samples: number[] = [];
+  for (let i = 0; i < 2 * n; i++) { samples.push(0); }
+
+  for (let i = 0; i < 2 * n; i++) {
+    for (let k = 0; k < n; k++) {
+      samples[i] += frequencies[k] * Math.cos(Math.PI / n * (i + (1 + n) / 2) * (k + 0.5));
+    }
+  }
+  for (let i = 0; i < 2 * n; i++) { samples[i] /= n; }
+
+  return samples;
+}
+*/
+
