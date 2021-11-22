7e54802ef291180f5896dab32757a8cf13cadcfb
monyone <monyone.teihen@gmail.com>
2021-11-23 02:27:09 +0900

    add DCT-IV

--- src/mdct.ts
+++ src/mdct.ts
@@ -1,5 +1,3 @@
-// とりあえずこれを使ってる: https://github.com/redlily/training-webaudio-compression
-
 const dctII = (x: number[]) => {
   const n = x.length;
   if (n === 1) { return x; }
@@ -19,46 +17,37 @@ const dctII = (x: number[]) => {
   }
 }
 
+// 1st algorighm
+const dctIV = (x: number[]) => {
+  const n = x.length;
+
+  for (let i = 0; i < n; i++) {
+    x[i] *= (2 * Math.cos(Math.PI * (2 * i + 1) / (4 * n)));
+  }
+
+  dctII(x);
+
+  x[0] /= 2;
+  for (let i = 1; i < n; i++) {
+    x[i] -= x[i - 1];
+  }
+
+  return x;
+}
+
 export const imdct = (frequencies: number[]) => {
   const n = frequencies.length;
-  const input = [];
-  for (let i = 0; i < n; i++) { input.push(frequencies[i]); }
+  const output = dctIV([...frequencies]).map((elem, index) => ((index < n / 2) ? -elem : elem) / n);
   const samples: number[] = [];
   for (let i = 0; i < 2 * n; i++) { samples.push(0); }
 
-  // cos値の変換用係数を掛け合わせ
-  let rad = Math.PI / (n << 2);
-  for (let i = 0; i < n; ++i) {
-    input[i] *= 2.0 * Math.cos(rad * ((i << 1) + 1));
-  }
-
-  // DCT-II
-  dctII(input);
-
-  // 差分方程式
-  input[0] *= 0.5;
-  let i = 0, j = 1;
-  const nh = n >> 1;
-  for (; i < nh; i = j++) {
-    input[j] += (input[i] = -input[i]);
-  }
-  for (; j < n; i = j++) {
-    input[j] -= input[i];
-  }
-
-  // スケーリング
-  for (let j = 0; j < n; j++) {
-    input[j] /= n;
-  }
-
-  // データを分離
   const ns1 = n - 1;            // n - 1
   const nd2 = n >> 1;           // n / 2
   const nm3d4 = n + nd2;        // n * 3 / 4
   const nm3d4s1 = nm3d4 - 1;    // n * 3 / 4 - 1
   for (let i = 0; i < nd2; i++) {
-    samples[ns1 - i] = -(samples[i] = input[nd2 + i]);
-    samples[nm3d4 + i] = (samples[nm3d4s1 - i] = input[i]);
+    samples[ns1 - i] = -(samples[i] = output[nd2 + i]);
+    samples[nm3d4 + i] = (samples[nm3d4s1 - i] = output[i]);
   }
 
   return samples;
